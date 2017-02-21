ae054587cf5e6e8580f0e2d646d9179c77ab31a6
Robert Allen <contact@robertdallen.com>
2017-02-21 01:02:22 +0000

    Mark as complete functionality

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
     
 before_action :set_todo_list
+before_action :set_todo_item, except: [:create]
 
 
 def create
@@ -9,7 +10,6 @@ def create
 end
 
 def destroy
-   @todo_item = @todo_list.todo_items.find(params[:id]) 
    if @todo_item.destroy
        flash[:success] = "Todo List Item was deleted"
    else
@@ -18,12 +18,23 @@ def destroy
    redirect_to @todo_list
 end
 
+def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo Item completed"
+end
+
 
 private
 
 def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
 end
+
+
+def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+end
+
     
 def todo_item_params
     params[:todo_item].permit(:content)
